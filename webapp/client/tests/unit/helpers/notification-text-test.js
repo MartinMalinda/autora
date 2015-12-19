53b14dc86d6f42ee8bbe98aef4da2b0b763c8dec
MartinMalinda <malindacz@gmail.com>
2015-12-19 23:17:58 +0100

    Generated tests

---
+++ webapp/client/tests/unit/helpers/notification-text-test.js
@@ -0,0 +1,10 @@
+import { notificationText } from '../../../helpers/notification-text';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | notification text');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = notificationText(42);
+  assert.ok(result);
+});
